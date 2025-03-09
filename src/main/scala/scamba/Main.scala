3416fae24135bf79c23d4cd7082a98fe4ddcc5aa
ajafri2001 <abdullahjafri2001@gmail.com>
2025-03-09 22:38:20 +0530

    updated list

--- src/main/scala/scamba/Main.scala
+++ src/main/scala/scamba/Main.scala
@@ -6,14 +6,4 @@ import scamba.collections.{list, $}
 
     val newList = ls where $.o % 2 == 0 as $.o + 3
 
-    val newList_1 = ls where { $.o % 2 == 0 } as { $.o + 3 }
-
-    val newList_2 = ls where {
-        val x = 2
-        $.o % x == 0
-    } as {
-        val x = 1
-        $.o + x
-    }
-
-    println(newList_2) // [3, 5, 7, 9, 11]
+    println(newList) // [3, 5, 7, 9, 11]
